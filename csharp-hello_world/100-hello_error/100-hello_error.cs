f9a8628e8aa5e34f74f7274e9a4d1312fd0f5870
Blandin3 <b.iradukund3@alustudent.com>
2025-09-14 11:07:23 +0200

    adding the advanced task

---
+++ csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class ErrorMessage
+{
+    static int Main(string[] args)
+    {
+        Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+        return 1; // exit with status code 1
+    }
+}
+
