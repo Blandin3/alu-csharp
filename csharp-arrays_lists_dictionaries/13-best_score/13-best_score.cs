582738e022a10dd4d2387e500489681e2d797a5a
Blandin3 <b.iradukund3@alustudent.com>
2025-09-21 00:40:18 +0200

    added remaining files

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return "None";
+
+        string bestKey = null;
+        int bestValue = -1;
+
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > bestValue)
+            {
+                bestValue = entry.Value;
+                bestKey = entry.Key;
+            }
+        }
+
+        return bestKey ?? "None";
+    }
+}
+
