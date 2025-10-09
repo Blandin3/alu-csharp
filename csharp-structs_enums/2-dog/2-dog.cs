c8427195a825d7ab2a975509048367b0f23ddd78
Blandin3 <b.iradukund3@alustudent.com>
2025-10-09 22:55:17 +0200

    all structs and enums tasks

--- csharp-structs_enums/2-dog/2-dog.cs
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+}
+
