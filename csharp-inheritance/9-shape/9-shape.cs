3f9986cca7a06c57bf372a0ed74ece65c67ed33d
Blandin3 <b.iradukund3@alustudent.com>
2025-11-01 17:36:00 -0700

    done all inheritance tasks and added a README.md file

---
+++ csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Square : Rectangle
+{
+    private int size;
+
+    public int Size
+    {
+        get => size;
+        set
+        {
+            if (value < 0) throw new ArgumentException("Size must be greater than or equal to 0");
+            size = value;
+            Width = value;
+            Height = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"[Square] {Size} / {Size}";
+    }
+}
+
+
