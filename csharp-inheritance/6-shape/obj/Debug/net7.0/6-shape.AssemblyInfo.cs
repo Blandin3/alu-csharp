7428efd6b4b96988694809963d70595e3661e613
Blandin3 <b.iradukund3@alustudent.com>
2025-11-02 02:14:51 -0800

    refined the last 4 tasks to pass the checker

--- csharp-inheritance/6-shape/obj/Debug/net7.0/6-shape.AssemblyInfo.cs
+++ csharp-inheritance/6-shape/obj/Debug/net7.0/6-shape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6-shape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02aab509db3b89628ee4937b05c3351db7c33541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9986cca7a06c57bf372a0ed74ece65c67ed33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("6-shape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6-shape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
