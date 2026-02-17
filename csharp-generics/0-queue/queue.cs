b7f3f6552dfffa37c50de0b330bef6bbaf65c79b
Blandin3 <b.iradukund3@alustudent.com>
2026-02-17 14:05:19 +0200

    refined the files

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,7 +1,15 @@
 using System;
 
+/// <summary>
+/// Generic Queue class
+/// </summary>
+/// <typeparam name="T">Type of elements in the queue</typeparam>
 public class Queue<T>
 {
+    /// <summary>
+    /// Returns the type of the Queue
+    /// </summary>
+    /// <returns>The type T of the Queue</returns>
     public Type CheckType()
     {
         return typeof(T);
